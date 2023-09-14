8bfa594e0a7b2acf362c470cbd75bd3449d79eae
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-09-14 21:02:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/iwhiwh/SEND-ALL-FILE-BOT /SEND-ALL-FILE-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /SEND-ALL-FILE-BOT
 fi
-cd /Auto-search-tamil-bot
+cd /SEND-ALL-FILE-BOT
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot....✅"
 python3 bot.py
