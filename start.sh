d80e97bc5f5251d8e774fa80585c33c8fa9eda6b
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-09-19 16:14:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iwhiwh/SEND-ALL-FILE-BOT /SEND-ALL-FILE-BOT
+  git clone https://github.com/iwhiwh/Noo_Name_Bot /Noo_Name_Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /SEND-ALL-FILE-BOT
+  git clone $UPSTREAM_REPO /Noo_Name_Bot
 fi
-cd /SEND-ALL-FILE-BOT
+cd /Noo_Name_Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....✅"
 python3 bot.py
